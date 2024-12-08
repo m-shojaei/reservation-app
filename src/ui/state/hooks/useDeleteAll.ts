907b3b18404187c1befa4498da0acf525b1f4aa5
Nader Shojaei <m.shojaei.eus@gmail.com>
2024-12-08 20:40:28 +0100

    cleanup URL in useDeleteAll

--- src/ui/state/hooks/useDeleteAll.ts
+++ src/ui/state/hooks/useDeleteAll.ts
@@ -1,10 +1,14 @@
 import { Dispatch } from "react";
 import { Action } from "..";
 import { removeAllReservations } from "../../../database";
+import { useSearchParams } from "react-router";
 
 export function useDeleteAll(dispatch: Dispatch<Action>) {
+  const [, setSearchParams] = useSearchParams();
   return async () => {
     await removeAllReservations();
     dispatch({ type: "RESET" });
+
+    setSearchParams({});
   };
 }
